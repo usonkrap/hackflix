c5e8906103c75239e003d4858ce301ceba5e54bf
PARK Nosu <usonkrap@gmail.com>
2019-02-24 01:37:27 +0900

    React Router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Home from 'Routes/Home';
 import TV from 'Routes/TV';
 import Search from 'Routes/Search';
 
 export default () => (
-    <Router>
-        <>
-            <Route path='/' exact component={Home} />
-            <Route path='/tv' component={TV} />
-            <Route path='/search' component={Search} />
-        </>
-    </Router>
+	<Router>
+		<Switch>
+			<Route path="/" exact component={Home} />
+			<Route path="/tv" component={TV} />
+			<Route path="/search" component={Search} />
+			<Redirect from="*" to="/" />
+		</Switch>
+	</Router>
 );
