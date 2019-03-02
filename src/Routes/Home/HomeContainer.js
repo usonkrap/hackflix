7200e190c03c39c7abf83db6f08a26874cbd758e
PARK Nosu <usonkrap@gmail.com>
2019-03-02 10:47:58 +0900

    Home Container

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePresenter from './HomePresenter';
+import { moviesApi } from 'api';
 
 export default class extends React.Component {
 	state = {
@@ -9,6 +10,27 @@ export default class extends React.Component {
 		error: null,
 		loading: true
 	};
+
+	async componentDidMount() {
+		try {
+			const { data: { results: nowPlaying } } = await moviesApi.nowPlaying();
+			const { data: { results: popular } } = await moviesApi.popular();
+			const { data: { results: upcoming } } = await moviesApi.upcoming();
+			this.setState({
+				nowPlaying,
+				popular,
+				upcoming
+			});
+		} catch (error) {
+			this.setState({
+				error: "Can't find movies information."
+			});
+		} finally {
+			this.setState({
+				loading: false
+			});
+		}
+	}
 
 	render() {
 		const { nowPlaying, popular, upcoming, error, loading } = this.state;
