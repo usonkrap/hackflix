9bcd419f392d0261ac30ae9d59eb113bf6d90ca1
PARK Nosu <usonkrap@gmail.com>
2019-03-02 20:12:55 +0900

    Presenter Structure

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,16 @@
-export default () => 'search';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Styled from 'styled-components';
+
+const SearchPresenter = ({ movieResults, tvResults, searchTerm, loading, error, handleSubmit }) => null;
+
+SearchPresenter.propTypes = {
+	movieResults: PropTypes.array,
+	tvResults: PropTypes.array,
+	searchTerm: PropTypes.string,
+	loading: PropTypes.bool.isRequired,
+	error: PropTypes.string,
+	handleSubmit: PropTypes.func.isRequired
+};
+
+export default SearchPresenter;
