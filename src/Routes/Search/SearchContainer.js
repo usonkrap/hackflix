e458fec48b6569b0e8fceed089acbc35c70b6ceb
PARK Nosu <usonkrap@gmail.com>
2019-03-02 13:17:07 +0900

    Search Container

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import SearchPresenter from './SearchPresenter';
+import { moviesApi, tvApi } from 'api';
 
 export default class extends React.Component {
 	state = {
 		movieResults: null,
 		tvResults: null,
-		searchTerm: null,
+		searchTerm: '',
 		loading: false,
 		error: null
 	};
+
+	handleSubmit = () => {
+		const { searchTerm } = this.state;
+		if (searchTerm !== '') {
+			this.searchByTerm(searchTerm);
+		}
+	};
+
+	searchByTerm = async searchTerm => {
+		this.setState({ loading: true });
+		try {
+			const { data: { results: movieResults } } = await moviesApi.search(searchTerm);
+			const { data: { results: tvResults } } = await tvApi.search(searchTerm);
+			this.setState({
+				movieResults,
+				tvResults
+			});
+		} catch {
+			this.setState({ error: "Can't find results." });
+		} finally {
+			this.setState({ loading: false });
+		}
+	};
+
 	render() {
 		const { movieResults, tvResults, searchTerm, loading, error } = this.state;
 		return (
@@ -18,6 +43,7 @@ export default class extends React.Component {
 				searchTerm={searchTerm}
 				loading={loading}
 				error={error}
+				handleSubmit={this.handleSubmit}
 			/>
 		);
 	}
