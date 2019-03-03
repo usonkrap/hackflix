d78a3748b5e660860ad9fdbec2a5aabf243b21dd
PARK Nosu <usonkrap@gmail.com>
2019-03-03 18:49:51 +0900

    Poster Styled

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -1,45 +1,82 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  font-size: 12px;
+`;
 
-const ImageContainer = styled.div``;
+const Image = styled.div`
+  background-image: url(${prop => prop.bgUrl});
+  height: 180px;
+  background-size: cover;
+  border-radius: 5px;
+  background-position: center center;
+`;
+const Rating = styled.span`
+  bottom: 5px;
+  right: 5px;
+  position: absolute;
+  opacity: 0;
+  transition: opacity 0.1s linear;
+`;
+const ImageContainer = styled.div`
+  margin-bottom: 5px;
+  position: relative;
+  &:hover {
+    ${Image} {
+      opacity: 0.35;
+    }
+    ${Rating} {
+      opacity: 1;
+    }
+  }
+`;
 
-const Image = styled.div``;
+const Title = styled.span`
+  display: block;
+  margin-bottom: 3px;
+`;
 
-const Title = styled.span``;
-
-const Rating = styled.span``;
-
-const Year = styled.span``;
+const Year = styled.span`
+  font-size: 10px;
+  color: rgb(255, 255, 255, 0.5);
+`;
 
 const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
-	<Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
-		<Container>
-			<ImageContainer>
-				<Image bgUrl={imageUrl} />
-				<Rating>
-					<span role='img' aria-label='Rating'>
-						⭐️
-					</span>{' '}
-					{rating}/10
-				</Rating>
-			</ImageContainer>
-			<Title>{title}</Title>
-			<Year>{year}</Year>
-		</Container>
-	</Link>
+  <Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
+    <Container>
+      <ImageContainer>
+        <Image
+          bgUrl={
+            imageUrl
+              ? `https://image.tmdb.org/t/p/w300${imageUrl}`
+              : require("../assets/ImgNotFound.jpg")
+          }
+        />
+        <Rating>
+          <span role='img' aria-label='Rating'>
+            ⭐️
+          </span>{" "}
+          {rating}/10
+        </Rating>
+      </ImageContainer>
+      <Title>
+        {title.length > 18 ? `${title.substring(0, 18)}...` : title}
+      </Title>
+      <Year>{year}</Year>
+    </Container>
+  </Link>
 );
 
 Poster.propTypes = {
-	id: PropTypes.number.isRequired,
-	imageUrl: PropTypes.string,
-	title: PropTypes.string.isRequired,
-	rating: PropTypes.number,
-	year: PropTypes.string,
-	isMovie: PropTypes.bool
+  id: PropTypes.number.isRequired,
+  imageUrl: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+  year: PropTypes.string,
+  isMovie: PropTypes.bool
 };
 
 export default Poster;
